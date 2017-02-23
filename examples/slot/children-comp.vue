527bee2938021660a1c8f21971cbe829bd73cb76
longze <longzeee@gmail.com>
2017-02-23 10:16:07 +0800

    添加 example / slot

---
+++ examples/slot/children-comp.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <slot></slot>
+        <slot name="t2"></slot>
+        <slot name="t3" text3="作用域是子组件"></slot>
+        <slot name="list" :list="list" title="子组件数据作用域下的循环："></slot>
+    </div>
+</template>
+<script>
+    module.exports = {
+        data: function () {
+            return {
+                list: [
+                    {
+                        name: 'name1，'
+                    },
+                    {
+                        name: 'name2'
+                    }
+                ]
+            };
+        }
+    };
+</script>
--- examples/slot/index.vue
+++ examples/slot/index.vue
@@ -1,17 +1,35 @@
 <template>
     <div>
-        <parents>
-            <div>slot in index</div>
-        </parents>
+        <children-comp>
+            <div>{{text1}}</div>
+            <div slot="t2">{{text2}}</div>
+            <template slot="t3" scope="props">
+                <!-- 注意上面一行必须用 template 标签 -->
+                <div>
+                    <span>{{ props.text3 }}</span>
+                </div>
+            </template>
+            <template slot="list" scope="props">
+                <div>
+                    {{props.title}}
+                    <span v-for="item in props.list">{{ item.name }}</span>
+                </div>
+            </template>
+
+        </children-comp>
     </div>
 </template>
 <script>
     module.exports = {
         data: function () {
-            return {};
+            return {
+                text1: '普通单个匿名 slot，作用域是父组件',
+                text2: '具名组件，作用域是父组件',
+                text3: 'text3，作用域是父组件' // 上面 Dom 中的 text3 并不会取该值
+            };
         },
         components: {
-            'parents': require('./parents.vue')
+            'children-comp': require('./children-comp.vue')
         }
     };
 </script>
