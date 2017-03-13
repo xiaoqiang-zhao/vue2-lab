a312cf57dd0befa2462da80ed35e8705aafb18b7
longze <longzeee@gmail.com>
2017-03-13 21:58:12 +0800

    添加 mock 功能

--- build/server.js
+++ build/server.js
@@ -2,19 +2,42 @@
  * Created by zhaoxiaoqiang on 2017/3/10.
  */
 
-const config = {
-    port: 4000
-};
+/**
+ * Module dependencies.
+ */
 
 const Koa = require('koa');
 const app = new Koa();
-var koaStatic = require('koa-static');
-var koaAutoPathRouter = require('./koa-auto-path-router');
+const koaStatic = require('koa-static');
+const koaAutoPathRouter = require('./koa-auto-path-router');
+const koaProxy = require('koa-proxy');
+
+/**
+ * Module config.
+ */
+
+const config = {
+    port: 4000,
+    index: 'index.html',
+    // 是否开启远程代理，用于联调
+    isProxy: false,
+    proxyUrl: 'http://127.0.0.1:5000',
+    mockUrl: './mock/'
+};
 
 // 静态文件支持
 app.use(koaStatic('./dist/', {
-    // index: 'index.html'
+    index: config.index
 }));
+
+if (config.isProxy) {
+    app.use(koaProxy({
+        host: config.proxyUrl
+    }));
+}
+else {
+    app.use(koaAutoPathRouter(config.mockUrl));
+}
 
 app.listen(config.port);
 
---
+++ mock/GET/a/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by zhaoxiaoqiang on 2017/3/13.
+ */
+module.exports = function (params, req) {
+    return {
+        "statusInfo": "success",
+        "status": 0,
+        "data": {
+            "text": 'mock 数据'
+        }
+    };
+};
